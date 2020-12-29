a3f6e62ab325834fc254ad852b633c2cd3cb9e87
zhangkai <2925544651@qq.com>
2020-12-29 23:24:10 +0800

    修改了视图函数

--- aoe/views.py
+++ aoe/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 def index(request):
     print('第一个视图函数')
     return HttpResponse('呵呵呵')
+
+def login(requst):
+    print('第二个视图函数')
+    return HttpResponse('heiheihie')
+
+def book(request):
+    print('这是第三个视图函数')
+    return HttpResponse('去问问请问的')
